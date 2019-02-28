d6b59f3b08a5609c16ee8f0d79c60d60a4a678cb
Kleveland Ranapia <30019853@student.toiohomai.ac.nz>
2019-02-28 13:51:18 +1300

    kleveland

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your first name");
+            string fname = Console.ReadLine();
+
+            Console.WriteLine("please enter your last name");
+            string lname = Console.ReadLine();
+            /*string concantination*/
+
+            Console.WriteLine(fname+ " " +lname);
+            Console.ReadLine();
+
+            Console.WriteLine("please enter your name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("please enter your address");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("please enter your age");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("please enter your email address");
+            String email = Console.ReadLine();
+
+            Console.WriteLine("please enter your 4-digit PIN number");
+            int PIN = int.Parse (Console.ReadLine());
+
+        }
+    }
+}
